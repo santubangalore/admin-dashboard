d55f7e4fa230eb0949189cf8573594f70ac33f40
santu ghosh <santutsk@gmail.com>
2026-07-25 02:25:22 +0530

    quick-serve dashboard first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,17 @@ const Header = () => {
         border-slate-200/50 dark:border-slate-700/50"> 
         <div className="flex items-center justify-between ">
             <div className="flex items-center space-x-4">
-                <button > 
+                <button className="p-2 rounded-lg text-slate-600 dark:text-slate-200
+                    hover:bg-slate-100 hover:cursor-pointer"> 
                     <Menu className="h-5 w-5"></Menu>
                 </button>
+                <div className="hidden md:block">
+                    <h1 className="text-xl font-black text-slate-800 dark:text-white ">Dashboard</h1>
+                    <p>Welcome back Alex! Here's what's happenning today.</p>
+                </div>
+            </div>
+            <div>
+                
             </div>
         </div>
     </div>
